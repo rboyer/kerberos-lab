a7c90a9eb89f4a751c1baa80edf4a262c6cf57b6
R.B. Boyer <rb@hashicorp.com>
2018-12-17 15:56:42 -0600

    demo go program

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/rboyer/kerberos-lab
+
+require gopkg.in/jcmturner/gokrb5.v6 v6.1.0
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	// _ "github.com/jcmturner/gokrb5"
+	_ "gopkg.in/jcmturner/gokrb5.v6"
+
+	"fmt"
+)
+
+func main() {
+	fmt.Println("vim-go")
+}
